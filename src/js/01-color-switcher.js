c67520502886dff7fcb2a9bf10bb0f34aae23440
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-08-22 23:35:04 +0300

    did 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onButtonStart = function ({ target }) {
+  timerId = setInterval(() => {
+    target.parentNode.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  target.disabled = true;
+};
+
+const onButtonStop = function () {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+};
+
+buttonStop.addEventListener('click', onButtonStop);
+buttonStart.addEventListener('click', onButtonStart);
