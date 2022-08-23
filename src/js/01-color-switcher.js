ecc1e028aa44c72f98ab6ff2a26e6b30e204fa1d
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-08-23 13:14:31 +0300

    fix timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,6 @@
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
-
+let timerId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
