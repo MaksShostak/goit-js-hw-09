1c57d481e376e36aa85d972ae34d208e2eba7685
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-08-23 13:34:43 +0300

    fix bags task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,44 @@
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 let timerId = null;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+buttonStop.addEventListener('click', onButtonStop);
+buttonStart.addEventListener('click', onButtonStart);
 
-const onButtonStart = function ({ target }) {
-  timerId = setInterval(() => {
-    target.parentNode.style.backgroundColor = getRandomHexColor();
-  }, 1000);
+// 1варіант
+
+function backgroundColorBodyChange(target) {
+  target.parentNode.style.backgroundColor = getRandomHexColor();
+}
+
+function onButtonStart({ target }) {
+  timerId = setInterval(backgroundColorBodyChange, 1000, target);
   target.disabled = true;
   buttonStop.disabled = false;
-};
+}
 
-const onButtonStop = function ({ target }) {
+function onButtonStop({ target }) {
   clearInterval(timerId);
   buttonStart.disabled = false;
   target.disabled = true;
   //   target.parentNode.removeAttribute('style');
-};
+}
+// 2варіант
 
-buttonStop.addEventListener('click', onButtonStop);
-buttonStart.addEventListener('click', onButtonStart);
+// function onButtonStart({ target }) {
+//   timerId = setInterval(() => {
+//     target.parentNode.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+//   target.disabled = true;
+//   buttonStop.disabled = false;
+// }
+
+// function onButtonStop({ target }) {
+//   clearInterval(timerId);
+//   buttonStart.disabled = false;
+//   target.disabled = true;
+//   //   target.parentNode.removeAttribute('style');
+// }
